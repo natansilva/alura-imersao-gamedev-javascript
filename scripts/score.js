1600d67039fd824ded1dac2848ccc4f888cb48ab
Natan Anael <natan.anael@gmail.com>
2020-06-25 15:15:57 -0300

    Fix socre speed

--- scripts/score.js
+++ scripts/score.js
@@ -30,24 +30,7 @@ class Score {
     }
 
     play(){
-        image(
-            this.numbersImage, 100, 0, 50, 60,
-            this.numberMatrix[this.placar1].x, this.numberMatrix[this.placar1].y,
-            this.numberMatrix[this.placar1].sx, this.numberMatrix[this.placar1].sy);
-        
-        image(
-            this.numbersImage, 50, 0, 50, 60,
-            this.numberMatrix[this.placar2].x, this.numberMatrix[this.placar2].y,
-            this.numberMatrix[this.placar2].sx, this.numberMatrix[this.placar2].sy);
-
-        image(
-            this.numbersImage, 0, 0, 50, 60,
-            this.numberMatrix[this.placar3].x, this.numberMatrix[this.placar3].y,
-            this.numberMatrix[this.placar3].sx, this.numberMatrix[this.placar3].sy);
-
-        this.placar1++;
-
-        if (this.placar1 == 10) {
+        if (int(this.placar1) == 10) {
             this.placar1 = 0;
             this.placar2++;
         }
@@ -62,5 +45,22 @@ class Score {
             this.placar2 = 0;
             this.placar3 = 0;
         }
+
+        image(
+            this.numbersImage, 100, 0, 50, 60,
+            this.numberMatrix[int(this.placar1)].x, this.numberMatrix[int(this.placar1)].y,
+            this.numberMatrix[int(this.placar1)].sx, this.numberMatrix[int(this.placar1)].sy);
+        
+        image(
+            this.numbersImage, 50, 0, 50, 60,
+            this.numberMatrix[this.placar2].x, this.numberMatrix[this.placar2].y,
+            this.numberMatrix[this.placar2].sx, this.numberMatrix[this.placar2].sy);
+
+        image(
+            this.numbersImage, 0, 0, 50, 60,
+            this.numberMatrix[this.placar3].x, this.numberMatrix[this.placar3].y,
+            this.numberMatrix[this.placar3].sx, this.numberMatrix[this.placar3].sy);
+
+        this.placar1 = this.placar1 + 0.1;
     }
 }
