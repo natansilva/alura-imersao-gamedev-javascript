0de84c6539a56d7530222626e67bb12d55a7173e
Natan Anael <natan.anael@gmail.com>
2020-06-25 14:07:55 -0300

    Add troll and flight blue enemies

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,7 +1,11 @@
 let character;
 let characterImage;
-let characterEnemie;
-let characterEnemieImage;
+let blueCharacterEnemie;
+let blueCharacterEnemieImage;
+let flightBlueCharacterEnemie;
+let flightBlueCharacterEnemieImage;
+let trollCharacterEnemie;
+let trollCharacterEnemieImage;
 let ground;
 let hillScenario;
 let score;
@@ -11,10 +15,12 @@ let treeScenario;
 
 function preload(){
   characterImage = loadImage('images/character/running.png');
-  characterEnemieImage = loadImage('images/enemies/blue-enemies.png');
+  blueCharacterEnemieImage = loadImage('images/enemies/blue-enemies.png');
+  flightBlueCharacterEnemieImage = loadImage('images/enemies/flight-blue-enemies.png');
+  trollCharacterEnemieImage = loadImage('images/enemies/troll.png');
   gameOverImage = loadImage('images/assets/game-over.png');
   // audioGame = loadSound('sons/trilha_jogo.mp3');
-  frameRate(25);
+  frameRate(60);
 }
 
 function setup() {
@@ -25,7 +31,10 @@ function setup() {
   ground = hillScenario.ground;
 
   character = new Character(characterImage, 135, 110, 220, 270, 100, height-ground-110, 4, 4, 'row');
-  characterEnemie = new Character(characterEnemieImage, 52, 52, 104, 104, width-52, height-ground-52, 4, 7, 'column');
+  blueCharacterEnemie = new Character(blueCharacterEnemieImage, 52, 52, 104, 104, width-52, height-ground-52, 4, 7, 'column');
+  flightBlueCharacterEnemie = new Character(flightBlueCharacterEnemieImage, 100, 75, 200, 150, width+100-52, height/3-ground-52, 5, 3, 'row')
+  flightBlueCharacterEnemie2 = new Character(flightBlueCharacterEnemieImage, 100, 75, 200, 150, width+500-52, height/3-ground-52, 5, 3, 'row')
+  trollCharacterEnemieImage = new Character(trollCharacterEnemieImage, 200, 230, 400, 400, width, height-ground-200, 5, 5, 'row');
   // audioGame.loop();
 
   score = new Score();
@@ -37,7 +46,7 @@ function keyPressed(){
   }
 
   if(key == 'Enter'){
-    characterEnemie.restart();
+    blueCharacterEnemie.restart();
     character.restart();
     score.restart();
     loop();
@@ -45,19 +54,31 @@ function keyPressed(){
 }
 
 function draw() {
-  treeScenario.show();
+  // treeScenario.show();
   hillScenario.show();
   
-  characterEnemie.show();
-  characterEnemie.animate();
-  characterEnemie.play(10);
+  blueCharacterEnemie.show();
+  blueCharacterEnemie.animate();
+  blueCharacterEnemie.play(5);
+
+  flightBlueCharacterEnemie.show();
+  flightBlueCharacterEnemie.animate();
+  flightBlueCharacterEnemie.play(5);
+
+  flightBlueCharacterEnemie2.show();
+  flightBlueCharacterEnemie2.animate();
+  flightBlueCharacterEnemie2.play(5);
+
+  trollCharacterEnemieImage.show();
+  trollCharacterEnemieImage.animate();
+  trollCharacterEnemieImage.play(5);
 
   character.show();
   character.animate();
   character.play(0);
   score.play();
   
-  if (character.isCollide(characterEnemie)) {
+  if (character.isCollide(blueCharacterEnemie)) {
     image(gameOverImage, width/2-205, height/2-37);
     noLoop();
   }
